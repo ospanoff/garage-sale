f36acab9ec892e9ac274fe3a200e6bb6218573cd
Ayat Ospanov <ayat.ospanov@gmail.com>
2022-03-26 16:54:04 +0100

    Initialized firebase
    Added Firestore and Cloud functions support

---
+++ src/config.ts
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: import.meta.env.API_KEY,
+  authDomain: "garage-sale-a45ef.firebaseapp.com",
+  projectId: "garage-sale-a45ef",
+  storageBucket: "garage-sale-a45ef.appspot.com",
+  messagingSenderId: "537183742815",
+  appId: "1:537183742815:web:5652c32a44df062ac4a4ea",
+};
+
+export default firebaseConfig;
